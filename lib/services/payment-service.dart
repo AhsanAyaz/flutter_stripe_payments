c93ebf5cbff7c240ad2dd08964084a1d7558208e
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-04-13 14:55:40 +0200

    feat(stripe): pay with a new card
    Added functionality for paying with a new card
    Showed appropriate success/error message in the case above

--- lib/services/payment-service.dart
+++ lib/services/payment-service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:stripe_payment/stripe_payment.dart';
 
 class StripeTransactionResponse {
   String message;
@@ -6,11 +9,21 @@ class StripeTransactionResponse {
 }
 
 class StripeService {
-  static String apiBase = 'https://api.stripe.com//v1';
-  static String secret = '';
-
+  static String apiBase = 'https://api.stripe.com/v1';
+  static String paymentApiUrl = '${StripeService.apiBase}/payment_intents';
+  static String secret = 'your_api_secret';
+  static Map<String, String> headers = {
+    'Authorization': 'Bearer ${StripeService.secret}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
   static init() {
-    
+    StripePayment.setOptions(
+      StripeOptions(
+        publishableKey: "your_api_publishable_key",
+        merchantId: "Test",
+        androidPayMode: 'test'
+      )
+    );
   }
 
   static StripeTransactionResponse payViaExistingCard({String amount, String currency, card}) {
@@ -20,10 +33,56 @@ class StripeService {
     );
   }
 
-  static StripeTransactionResponse payWithNewCard({String amount, String currency}) {
-    return new StripeTransactionResponse(
-      message: 'Transaction successful',
-      success: true
-    );
+  static Future<StripeTransactionResponse> payWithNewCard({String amount, String currency}) async {
+    try {
+      var paymentMethod = await StripePayment.paymentRequestWithCardForm(
+        CardFormPaymentRequest()
+      );
+      var paymentIntent = await StripeService.createPaymentIntent(
+        amount,
+        currency
+      );
+      var response = await StripePayment.confirmPaymentIntent(
+        PaymentIntent(
+          clientSecret: paymentIntent['client_secret'],
+          paymentMethodId: paymentMethod.id
+        )
+      );
+      if (response.status == 'succeeded') {
+        return new StripeTransactionResponse(
+          message: 'Transaction successful',
+          success: true
+        );
+      } else {
+        return new StripeTransactionResponse(
+          message: 'Transaction failed}',
+          success: false
+        );
+      }
+    } catch (err) {
+      return new StripeTransactionResponse(
+        message: 'Transaction failed: ${err.toString()}',
+        success: false
+      );
+    }
+  }
+
+  static Future<Map<String, dynamic>> createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': amount,
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+      var response = await http.post(
+        StripeService.paymentApiUrl,
+        body: body,
+        headers: StripeService.headers
+      );
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charing user: ${err.toString()}');
+    }
+    return null;
   }
 }
